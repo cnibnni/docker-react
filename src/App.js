9134e432cc7a909c567260b9016a5405f7a9a5b1
Ogulcan Özünal <ogulcan.oezuenal@ausy-technologies.de>
2021-06-08 13:44:57 +0200

    changed app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>change this pls now</p>
+        <p>i was changed on the feature branch</p>
         <a
           className="App-link"
           href="https://reactjs.org"
